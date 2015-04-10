27591142442efcf60a685ae1a832f4d819dc124c
semho <semho@yandex.ru>
2015-04-10 15:06:40 +0300

    Добавлен класс News с предком Articles

---
+++ lesson_2/model/News.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Администратор
+ * Date: 10.04.2015
+ * Time: 15:05
+ */
+
+class News extends Articles{
+    public function allNews(){
+
+    }
+    public function selectOneById($id){
+        
+    }
+} 
