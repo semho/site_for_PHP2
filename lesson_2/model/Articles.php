4ac0a8555153aa89fd59ea91068a27841e4b10b6
semho <semho@yandex.ru>
2015-04-10 15:04:57 +0300

    Добавлен абстрактный класс

---
+++ lesson_2/model/Articles.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Администратор
+ * Date: 10.04.2015
+ * Time: 15:02
+ */
+
+abstract class Articles {
+
+    abstract public function allNews();
+    abstract public function selectOneById($id);
+} 
